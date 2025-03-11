f9a8f7c826da9fca452bb16dca9b0a94ddbf92fc
KlausStille <mail@klausstille.com>
2025-03-11 09:36:17 +0100

    fix/mobile menu

--- components/BackgroundLine/BackgroundLine.tsx
+++ components/BackgroundLine/BackgroundLine.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
 import "./BackgroundLine.css";
 
 interface BackgroundLineProps {
@@ -11,6 +12,37 @@ export function BackgroundLine({
     isMenuOpen,
     isProjectsOpen,
 }: BackgroundLineProps) {
+    const prevMenuOpenRef = useRef(isMenuOpen);
+    const prevProjectsOpenRef = useRef(isProjectsOpen);
+
+    const [pathData, setPathData] = useState(`M15 0 L15 8 L305 8 L305 0`);
+
+    useEffect(() => {
+        if (
+            prevMenuOpenRef.current !== isMenuOpen ||
+            prevProjectsOpenRef.current !== isProjectsOpen
+        ) {
+            prevMenuOpenRef.current = isMenuOpen;
+            prevProjectsOpenRef.current = isProjectsOpen;
+
+            if (isMenuOpen) {
+                setPathData(`M15 6 L15 6 L305 6 L305 6`);
+                const timer = setTimeout(() => {
+                    setPathData(`M15 0 L15 90 L305 13 L305 600`);
+                }, 50);
+
+                return () => clearTimeout(timer);
+            } else {
+                setPathData(`M15 6 L15 20 L305 6 L305 6`);
+                const timer = setTimeout(() => {
+                    setPathData(`M15 6 L15 6 L305 6 L305 6`);
+                }, 50);
+
+                return () => clearTimeout(timer);
+            }
+        }
+    }, [isMenuOpen]);
+
     return (
         <div className="absolute inset-0 pointer-events-none pb-4">
             <svg
@@ -23,13 +55,11 @@ export function BackgroundLine({
                 className="w-full h-full"
             >
                 <path
-                    d={`M15 0 L15 ${isMenuOpen ? "90" : "8"} L305 ${
-                        isMenuOpen ? "13" : "8"
-                    }  L305 ${isMenuOpen ? "600" : "0"}`}
+                    d={pathData}
                     stroke="black"
-                    strokeWidth="1"
+                    strokeWidth="0.9"
                     fill="none"
-                    className="transition-all duration-700 delay-100 ease-in-out will-change-[d] motion-reduce:transition-none"
+                    className="svg-path"
                 />
             </svg>
         </div>
