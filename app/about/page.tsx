e9148dd9b64de0db855bf381d878be719f87dfac
KlausStille <mail@klausstille.com>
2025-03-22 14:54:19 +0100

    implementation client feedback 3.0

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,12 @@
-import { getAbout } from "@/lib/contentful";
+import { getAbout, getPageAnimations } from "@/lib/contentful";
 import { AboutComponent } from "@/components/About";
+import ClientAnimation from "@/components/ClientAnimation";
 
 export default async function AboutPage() {
     const about = await getAbout();
+    const { aboutPageAnimationMov, aboutPageAnimationWebm } =
+        await getPageAnimations();
+
     if (!about) {
         return;
     }
@@ -10,6 +14,10 @@ export default async function AboutPage() {
     return (
         <main className="sm:min-h-[calc(100vh-50px)] min-h-[calc(100vh-80px)] p-4 grid sm:grid-cols-12 grid-cols-1">
             <AboutComponent about={about} />
+            <ClientAnimation
+                movUrl={aboutPageAnimationMov?.url}
+                webmUrl={aboutPageAnimationWebm?.url}
+            />
         </main>
     );
 }
