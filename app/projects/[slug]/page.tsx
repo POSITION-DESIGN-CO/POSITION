01c1e893e8edde05d34878471874ddf00740e5a9
KlausStille <mail@klausstille.com>
2025-03-28 19:02:53 +0100

    add metadata to project detail

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,19 +1,74 @@
-// import Image from "next/image";
 import { getPageAnimations, getProjectBySlug } from "@/lib/contentful";
-// import Link from "next/link";
 import { headers } from "next/headers";
 import { ProjectDetail } from "@/components/Projects/ProjectDetail";
-// import ClientAnimation from "@/components/Animations/ClientAnimation";
-// import { useProjectsStore } from "@/store";
+import { Metadata } from "next";
+import { Project } from "@/lib/contentful-models";
 
-// interface ImageProps {
-//     title: string;
-//     url: string;
-//     blurDataURL?: string;
-//     width: number;
-//     height: number;
-//     sys: { id: string };
-// }
+type Props = {
+    params: { slug: string };
+    searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const { slug } = params;
+    const foundProject: Project = await getProjectBySlug(slug);
+
+    if (!foundProject) {
+        return {
+            title: "Project not found",
+        };
+    }
+
+    const description = foundProject.description;
+    const primaryImage = foundProject.thumbnail;
+
+    const maxWidth = 800;
+    const maxHeight = 420;
+
+    let width = primaryImage?.width;
+    let height = primaryImage?.height;
+
+    if (width && height) {
+        if (width > maxWidth) {
+            const aspectRatio = height / width;
+            width = maxWidth;
+            height = Math.round(maxWidth * aspectRatio);
+        }
+        if (height > maxHeight) {
+            const aspectRatio = width / height;
+            height = maxHeight;
+            width = Math.round(maxHeight * aspectRatio);
+        }
+    }
+
+    return {
+        title: foundProject.title,
+        description: description,
+        twitter: {
+            card: "summary_large_image",
+            title: foundProject.title,
+            description: description,
+            images: [
+                {
+                    url: primaryImage.url,
+                    width: width ?? maxWidth,
+                    height: height ?? maxHeight,
+                },
+            ],
+        },
+        openGraph: {
+            title: foundProject.title,
+            description: description,
+            images: [
+                {
+                    url: primaryImage.url,
+                    width: width ?? maxWidth,
+                    height: height ?? maxHeight,
+                },
+            ],
+        },
+    };
+}
 
 export default async function ProjectPage({
     params,
